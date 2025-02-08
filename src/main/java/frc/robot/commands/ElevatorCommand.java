bdac8e1f6f63af561827854d4b64c3082bdc064b
Athena Lieu <athenaaabelle@gmail.com>
2025-02-07 19:23:28 -0500

    Elevator Command + Subsystem clean-up

--- src/main/java/frc/robot/commands/ElevatorCommand.java
+++ src/main/java/frc/robot/commands/ElevatorCommand.java
@@ -4,13 +4,21 @@
 
 package frc.robot.commands;
 
+import frc.robot.subsystems.Elevator;
+import java.util.function.Supplier;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class ElevatorCommand extends Command {
+  private final Elevator elevator;
+  private Supplier<Double> elevatorSpeed;
+  
   /** Creates a new ElevatorCommand. */
-  public ElevatorCommand() {
+  public ElevatorCommand(Elevator elevator, Supplier<Double> elevatorSpeed) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.elevator = elevator;
+    this.elevatorSpeed = elevatorSpeed;
+    addRequirements(elevator);
   }
 
   // Called when the command is initially scheduled.
@@ -19,11 +27,13 @@ public class ElevatorCommand extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
--- src/main/java/frc/robot/commands/PivotCommand.java
+++ src/main/java/frc/robot/commands/PivotCommand.java
@@ -7,9 +7,9 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class GripperCommand extends Command {
+public class PivotCommand extends Command {
   /** Creates a new GripperCommand. */
-  public GripperCommand() {
+  public PivotCommand() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
