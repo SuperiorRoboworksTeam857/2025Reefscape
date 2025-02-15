4ea45f6e8b22ec5757269bf88ea623f07071ddc3
857 programming <csdoig@mtu.edu>
2025-02-14 19:51:20 -0500

    Add a stopIntake() function, TODO for scoring L4, remove other TODO

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -6,8 +6,7 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkFlex;
 public class Intake extends SubsystemBase {
   
-//TO DO - set intake constant to device id
-    private SparkFlex flexMotor = new SparkFlex(IntakeConstants.intakeMotor, MotorType.kBrushless);
+  private SparkFlex flexMotor = new SparkFlex(IntakeConstants.intakeMotor, MotorType.kBrushless);
 
   public Intake() {}
 
@@ -15,15 +14,20 @@ public class Intake extends SubsystemBase {
   public void periodic() {}
 
   public void runIntake(double speed) {
-flexMotor.set(speed*IntakeConstants.intakeSpeedMultiplier);
+    flexMotor.set(speed*IntakeConstants.intakeSpeedMultiplier);
   }
 
   public void intakeGamePiece() {
     runIntake(-1.0);
   }
 
+  // TODO: make aware of L4 because outtaking is normally same as intaking except at L4 wrist position
   public void outtakeGamePiece() {
     runIntake(1.0);
+  }
+
+  public void stopIntake() {
+    runIntake(0.0);
   }
 
 }
